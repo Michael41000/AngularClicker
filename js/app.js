0b4117477c760ebab29c7e7de91134f7478606e9
unknown <mrollberg@ymail.com>
2017-09-28 13:01:06 -0500

    Changed gow numbers are displayed

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 angular.module('clickerApp', ['ngCookies'])
+.filter('exponential', function() {
+    return function(x) {
+        if (x > 100000)
+        {
+            return x.toExponential(5)
+        }
+        else
+        {
+            return x.toFixed(2)
+        }
+        
+    }
+})
